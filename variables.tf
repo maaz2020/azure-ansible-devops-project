086d29cb85df60bb06de275d5f49ff6f550e426a
maaz2020 <maaz.amjad@gmail.com>
2024-11-09 21:31:40 +0530

    made changes to variables file

--- variables.tf
+++ variables.tf
@@ -1,5 +1,3 @@
 variable "azure_public_ssh_key" {
-  description = "Path to the Azure public SSH key"
-  type        = string
-  default     = "/C/devops/azure/azure_keys/ansible-server_key.pem"
+  default = "~/devops/azure/azure_keys/ansible-server_key.pem"
 }
