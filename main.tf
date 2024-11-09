56fcdc9b95bd388ae84f148e5c95d64be8f1a632
maaz2020 <maaz.amjad@gmail.com>
2024-11-09 21:23:36 +0530

    made changes to main file

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.0.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = ">=3.0.0"
+#     }
+#   }
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 # Resource Group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
