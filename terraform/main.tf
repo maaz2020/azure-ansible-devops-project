342f0fabc16a8c0735cc3969198d5b72e834223e
maaz2020 <maaz.amjad@gmail.com>
2024-11-12 18:53:15 +0530

    terraform initiated

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,7 +114,7 @@ resource "azurerm_linux_virtual_machine" "http_server" {
   name                = "httpServerVM-${count.index}"
   resource_group_name = azurerm_resource_group.microservice.name
   location            = azurerm_resource_group.microservice.location
-  size                = "Standard_B2ms" # Equivalent to t2.micro in AWS
+  size                = "Standard_B1s" # Equivalent to t2.micro in AWS
   admin_username      = "adminuser"
   # admin_password                  = "Maaz@1234"
   disable_password_authentication = true
@@ -136,9 +136,10 @@ resource "azurerm_linux_virtual_machine" "http_server" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
+
 
 }
 
